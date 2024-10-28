3686a4a98d49590d59fd689174cad4ee8563f634
jensc <jenscaa@stud.ntnu.no>
2024-10-28 14:12:20 +0100

    refactor(environment): fixed and simplified wrappers to environment

--- src/environment/environment.py
+++ src/environment/environment.py
@@ -1,38 +1,21 @@
-import gym
+import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 from nes_py.wrappers import JoypadSpace
-from wrappers import (
-    ClipRewardEnv,
-    FrameStack,
-    FrameToTensor,
-    GrayScaleObservation,
-    PixelNormalize,
-    ResizeObservation,
-    SkipFrame,
+
+from src.environment.wrappers import (
+    ActionRepeat,
+    ConvertToTensor,
+    NormalizePixels,
+    ObservationBuffer,
+    ResizeAndGrayscale,
 )
 
 
-def create_env(map="SuperMarioBros-v0", skip=4):
-    """Create a Super Mario environment with appropriate wrappers for RL."""
-    env = gym.make(map)
-    env = SkipFrame(env, skip=skip)
-    env = GrayScaleObservation(env)
-    env = ResizeObservation(env, shape=(84, 84))
-    env = PixelNormalize(env)
-    env = FrameStack(env, 4)
-    env = FrameToTensor(env)
-    env = ClipRewardEnv(env)
-    env = JoypadSpace(env, SIMPLE_MOVEMENT)
-    return env
-
-
-env = create_env()
-
-done = True
-for _step in range(5000):
-    if done:
-        state = env.reset()
-    state, reward, done, info = env.step(env.action_space.sample())
-    env.render()
-
-env.close()
+def create_env(map="SuperMarioBros-v0", action_repeat=4):
+    """Sets up the Super Mario Bros environment with customized wrappers."""
+    env = ActionRepeat(gym_super_mario_bros.make(map), action_repeat)
+    env = ResizeAndGrayscale(env)
+    env = ConvertToTensor(env)
+    env = ObservationBuffer(env, 4)
+    env = NormalizePixels(env)
+    return JoypadSpace(env, SIMPLE_MOVEMENT)
--- src/environment/wrappers.py
+++ src/environment/wrappers.py
@@ -1,228 +1,61 @@
-from collections import deque
+import collections
 
 import cv2
 import gym
 import numpy as np
-from gym import ObservationWrapper
 
 
-# TODO: Adapt if needed for RL
-class SkipFrame(gym.Wrapper):
-    """Wrapper to skip a specified number of frames."""
+class ActionRepeat(gym.Wrapper):
+    """Repeats the action for a specified number of frames (default = 4)."""
 
-    def __init__(self, env, skip) -> None:
-        """Initialize the SkipFrame wrapper.
-
-        Args:
-        ----
-            env (gym.Env): The environment to wrap.
-            skip (int): The number of frames to skip.
-
-        """
+    def __init__(self, env, repeat=4):
         super().__init__(env)
-        self._skip = skip
+        self.buffer = collections.deque(maxlen=2)
+        self.repeat = repeat
 
     def step(self, action):
-        """Repeat action for a given number of frames and accumulate rewards.
-
-        Args:
-        ----
-            action: The action to be repeated over the skipped frames.
-
-        Returns:
-        -------
-            state: The observation after the skipped frames.
-            total_reward: The total accumulated reward during the skipped frames.
-            done: Whether the episode has ended.
-            info: Additional information from the environment.
-
-        """
         total_reward = 0.0
         done = False
-        for _ in range(self._skip):
-            state, reward, done, info = self.env.step(action)
+        for _i in range(self.repeat):
+            observation, reward, done, info = self.env.step(action)
             total_reward += reward
+            self.buffer.append(observation)
             if done:
                 break
-        return state, total_reward, done, info
+        max_frame = np.max(np.stack(self.buffer), axis=0)
+        self.frame = max_frame.copy()
+        return max_frame, total_reward, done, info
 
 
-class GrayScaleObservation(ObservationWrapper):
-    """Convert observations from RGB to grayscale."""
+class ResizeAndGrayscale(gym.ObservationWrapper):
+    """Resizes and converts the observation to grayscale, outputting 84x84 images."""
 
-    def __init__(self, env) -> None:
-        """Initialize the GrayScaleObservation wrapper.
-
-        Args:
-        ----
-            env (gym.Env): The environment to wrap.
-
-        """
+    def __init__(self, env):
         super().__init__(env)
         self.observation_space = gym.spaces.Box(
-            low=0,
-            high=255,
-            shape=(self.observation_space.shape[0], self.observation_space.shape[1]),
-            dtype=np.uint8,
+            low=0, high=255, shape=(84, 84, 1), dtype=np.uint8
         )
 
     def observation(self, observation):
-        """Convert an RGB observation to grayscale.
+        return ResizeAndGrayscale.convert(observation)
 
-        Args:
-        ----
-            observation: The RGB observation from the environment.
-
-        Returns:
-        -------
-            The grayscale version of the observation.
-
-        """
-        return cv2.cvtColor(observation, cv2.COLOR_RGB2GRAY)
-
-
-class ResizeObservation(ObservationWrapper):
-    """Resize observations to a given shape."""
-
-    def __init__(self, env, shape) -> None:
-        """Initialize the ResizeObservation wrapper.
-
-        Args:
-        ----
-            env (gym.Env): The environment to wrap.
-            shape (tuple): The desired shape for resizing the observation (height, width).
-
-        """
-        super().__init__(env)
-        self.shape = shape
-        self.observation_space = gym.spaces.Box(
-            low=0, high=255, shape=(shape[0], shape[1], 1), dtype=np.uint8
-        )
-
-    def observation(self, observation):
-        """Resize the observation to the specified shape.
-
-        Args:
-        ----
-            observation: The observation from the environment.
-
-        Returns:
-        -------
-            The resized observation.
-
-        """
-        observation = cv2.resize(observation, self.shape, interpolation=cv2.INTER_AREA)
-        return observation
+    @staticmethod
+    def convert(frame):
+        if frame.size == 240 * 256 * 3:
+            img = np.reshape(frame, [240, 256, 3]).astype(np.float32)
+        else:
+            raise ValueError("Unknown resolution.")
+        img = img[:, :, 0] * 0.299 + img[:, :, 1] * 0.587 + img[:, :, 2] * 0.114
+        resized_img = cv2.resize(img, (84, 110), interpolation=cv2.INTER_AREA)
+        cropped_img = resized_img[18:102, :]
+        processed_frame = np.reshape(cropped_img, [84, 84, 1])
+        return processed_frame.astype(np.uint8)
 
 
-class PixelNormalize(ObservationWrapper):
-    """Normalize pixel values from 0-255 to 0-1."""
+class ConvertToTensor(gym.ObservationWrapper):
+    """Converts observations to tensor format suitable for PyTorch."""
 
     def __init__(self, env):
-        """Initialize the PixelNormalize wrapper.
-
-        Args:
-        ----
-            env (gym.Env): The environment to wrap.
-
-        """
-        super().__init__(env)
-        self.observation_space = gym.spaces.Box(
-            low=0.0, high=1.0, shape=env.observation_space.shape, dtype=np.float32
-        )
-
-    def observation(self, obs):
-        """Normalize the pixel values of the observation to the range [0, 1].
-
-        Args:
-        ----
-            obs: The observation from the environment.
-
-        Returns:
-        -------
-            The normalized observation.
-
-        """
-        return obs.astype(np.float32) / 255.0
-
-
-class FrameStack(gym.Wrapper):
-    """Stack the last `k` frames to give the agent a sense of motion."""
-
-    def __init__(self, env, k):
-        """Initialize the FrameStack wrapper.
-
-        Args:
-        ----
-            env (gym.Env): The environment to wrap.
-            k (int): The number of frames to stack.
-
-        """
-        super().__init__(env)
-        self.k = k
-        self.frames = deque([], maxlen=k)
-        shp = env.observation_space.shape
-        self.observation_space = gym.spaces.Box(
-            low=0,
-            high=255,
-            shape=(shp[0], shp[1], shp[2] * k),
-            dtype=env.observation_space.dtype,
-        )
-
-    def reset(self):
-        """Reset the environment and stack `k` initial frames.
-
-        Returns
-        -------
-            The stacked frames as the initial observation.
-
-        """
-        obs = self.env.reset()
-        for _ in range(self.k):
-            self.frames.append(obs)
-        return self._get_observation()
-
-    def step(self, action):
-        """Take a step in the environment and stack the frames.
-
-        Args:
-        ----
-            action: The action to be taken in the environment.
-
-        Returns:
-        -------
-            obs: The stacked frames after the action.
-            reward: The reward from the action.
-            done: Whether the episode has ended.
-            info: Additional information from the environment.
-
-        """
-        obs, reward, done, info = self.env.step(action)
-        self.frames.append(obs)
-        return self._get_observation(), reward, done, info
-
-    def _get_observation(self):
-        """Get the stacked observation.
-
-        Returns
-        -------
-            The concatenated frames.
-
-        """
-        return np.concatenate(list(self.frames), axis=-1)
-
-
-class FrameToTensor(ObservationWrapper):
-    """Convert frames to tensors and move the channel axis to the front."""
-
-    def __init__(self, env):
-        """Initialize the FrameToTensor wrapper.
-
-        Args:
-        ----
-            env (gym.Env): The environment to wrap.
-
-        """
         super().__init__(env)
         old_shape = self.observation_space.shape
         self.observation_space = gym.spaces.Box(
@@ -233,33 +66,34 @@ class FrameToTensor(ObservationWrapper):
         )
 
     def observation(self, observation):
-        """Move the channel axis to the front for compatibility with PyTorch.
-
-        Args:
-        ----
-            observation: The observation from the environment.
-
-        Returns:
-        -------
-            The observation with the channel axis moved to the front.
-
-        """
-        return np.moveaxis(observation, -1, 0)  # Move channel axis
+        return np.moveaxis(observation, 2, 0)
 
 
-class ClipRewardEnv(gym.RewardWrapper):
-    """Clip rewards to the range [-1, 1]."""
+class ObservationBuffer(gym.ObservationWrapper):
+    """Maintains a sliding window of the last n observations."""
 
-    def reward(self, reward):
-        """Clip the reward to the range [-1, 1].
+    def __init__(self, env, n_steps, dtype=np.float32):
+        super().__init__(env)
+        self.dtype = dtype
+        old_space = env.observation_space
+        self.observation_space = gym.spaces.Box(
+            old_space.low.repeat(n_steps, axis=0),
+            old_space.high.repeat(n_steps, axis=0),
+            dtype=dtype,
+        )
 
-        Args:
-        ----
-            reward: The raw reward from the environment.
+    def reset(self):
+        self.buffer = np.zeros_like(self.observation_space.low, dtype=self.dtype)
+        return self.observation(self.env.reset())
 
-        Returns:
-        -------
-            The clipped reward.
+    def observation(self, observation):
+        self.buffer[:-1] = self.buffer[1:]
+        self.buffer[-1] = observation
+        return self.buffer
 
-        """
-        return np.sign(reward)
+
+class NormalizePixels(gym.ObservationWrapper):
+    """Normalizes pixel values in the observation to a range of [0, 1]."""
+
+    def observation(self, obs):
+        return np.array(obs).astype(np.float32) / 255.0
