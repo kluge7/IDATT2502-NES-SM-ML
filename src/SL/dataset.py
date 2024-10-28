5f4de3750f1efc9aa0bdd422d4afd3d46bc6d6bf
heikkkk <henrik.teksle.sandok@hotmail.no>
2024-10-28 13:01:33 +0100

    feat: add function to save to csv

--- src/SL/dataset.py
+++ src/SL/dataset.py
@@ -1,4 +1,6 @@
+import csv
 import re
+from pathlib import Path
 
 
 def parse_filename_to_data(filename: str) -> list:
@@ -12,3 +14,20 @@ def parse_filename_to_data(filename: str) -> list:
     outcome = re.split(r"[.]", components[12])[1]  # 'text.outcome.png' -> outcome
 
     return [episode, world, level, frame, action, outcome]
+
+
+def append_data_to_csv(parsed_data: list, csv_file_path: str) -> None:
+    with open(csv_file_path, "a", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(parsed_data)
+
+
+def extract_and_save_data(main_directory: str, csv_file_path: str) -> None:
+    main_path = Path(main_directory)
+
+    for subdirectory in main_path.iterdir():
+        if subdirectory.is_dir() and subdirectory.name.endswith("win"):
+            for file in subdirectory.iterdir():
+                if file.is_file():
+                    parsed_data = parse_filename_to_data(file.name)
+                    append_data_to_csv(parsed_data, csv_file_path)
