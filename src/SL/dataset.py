4fa1eff4202844256de221dfa34363296b19c169
Anders H <andershvik@gmail.com>
2024-10-30 20:54:45 +0100

    Added images to tensor

--- src/SL/dataset.py
+++ src/SL/dataset.py
@@ -3,6 +3,9 @@ import os
 import re
 from pathlib import Path
 
+from PIL import Image
+from torchvision import transforms
+
 path = "C:/Users/ander/project-machine/IDATT2502-NES-SM-ML/data-smb/data-smb-1-1/Rafael_dp2a9j4i_e0_1-1_win"
 
 dir_list = os.listdir(path)
@@ -11,6 +14,10 @@ frames = []
 
 
 actions_set = {0: "A", 1: "up", 2: "left", 3: "B", 4: "right", 5: "down", 6: "select"}
+
+
+def get_actionset():
+    return actions_set
 
 
 def get_action_from_bit():
@@ -38,8 +45,20 @@ def get_frames():
         frames.append(frame)
 
 
+def image_to_tensor():
+    convert_tensor = transforms.ToTensor()
+    images_path = "C:/Users/ander/project-machine/IDATT2502-NES-SM-ML/data-smb/data-smb-1-1/Rafael_dp2a9j4i_e0_1-1_win"
+    for image_ in os.listdir(images_path):
+        full_image_path = os.path.join(images_path, image_)
+        image = Image.open(full_image_path)
+        image_tensor = convert_tensor(image)
+        return image_tensor
+
+
 get_actions()
 action_index = get_action_from_bit()
+get_frames()
+image_to_tensor()
 
 
 def parse_filename_to_data(filename: str) -> list:
