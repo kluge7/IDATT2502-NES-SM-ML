041d407eb75d270bebd606b2050c0211480bdb6e
heikkkk <henrik.teksle.sandok@hotmail.no>
2024-11-09 12:18:28 +0100

    feat: change frame resize

--- src/supervised/utils/dataset_utils.py
+++ src/supervised/utils/dataset_utils.py
@@ -79,9 +79,9 @@ def load_dataset(
     transform = transforms.Compose(
         [
             transforms.Grayscale(num_output_channels=1),
-            transforms.Resize((240, 256)),
+            transforms.Resize((84, 84)),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.5], std=[0.5]),
+            transforms.Normalize(mean=[0], std=[1]),
         ]
     )
 
